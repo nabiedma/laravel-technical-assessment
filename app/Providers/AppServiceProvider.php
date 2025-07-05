8643b866d8be3fe0e933dac01a8d46b3e36f7858
nabiedma <nabiedma@gmail.com>
2025-07-05 16:11:59 -0300

    chore: set AppServiceProvider configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+        Date::use(CarbonImmutable::class);
+        // Model::unguard();
+        Model::shouldBeStrict();
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
